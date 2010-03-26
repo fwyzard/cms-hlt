6885cf4334955f77fbef808b7883165b7254b921
Andrea Bocci <andrea.bocci@cern.ch>
2010-03-26 03:34:32 +0000

    add back "HLT_Mu0_L1MuOpen", "HLT_Mu0_Track0_Jpsi", "HLT_Mu3_Track0_Jpsi", "HLT_Mu3_L1MuOpen", "HLT_Mu5_L1MuOpen", "HLT_Mu5_Track0_Jpsi"

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-fix the ES prescales for 900 GeV collisions (again)
+add back "HLT_Mu0_L1MuOpen", "HLT_Mu0_Track0_Jpsi", "HLT_Mu3_Track0_Jpsi", "HLT_Mu3_L1MuOpen", "HLT_Mu5_L1MuOpen", "HLT_Mu5_Track0_Jpsi"
