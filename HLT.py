32136f7d5eb60d32f9adf27d317f0f3c2361bee4
Andrea Bocci <andrea.bocci@cern.ch>
2010-03-26 03:59:23 +0000

    seed HLT_MinBiasPixel_* paths with "L1_BscMinBiasOR_BptxPlusORMinus"

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-add back "HLT_Mu0_L1MuOpen", "HLT_Mu0_Track0_Jpsi", "HLT_Mu3_Track0_Jpsi", "HLT_Mu3_L1MuOpen", "HLT_Mu5_L1MuOpen", "HLT_Mu5_Track0_Jpsi"
+seed HLT_MinBiasPixel_* paths with "L1_BscMinBiasOR_BptxPlusORMinus"
