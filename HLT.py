0322f98287c6f2478a72d0f246519cc08d736dab
Andrea Bocci <andrea.bocci@cern.ch>
2010-03-31 02:18:23 +0000

    stop using L1_SingleEG1, L1_SingleTauJet2U and L1_MinBias_HTT10 as seeds, and drop HLT_ActivityHF*, HLT_L1SingleEG1*, HLT_MultiTowerEcal, HLT_MinBias paths

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-drop L1_SingleTauJet2U seeding
+stop using L1_SingleEG1, L1_SingleTauJet2U and L1_MinBias_HTT10 as seeds, and drop HLT_ActivityHF*, HLT_L1SingleEG1*, HLT_MultiTowerEcal, HLT_MinBias paths
