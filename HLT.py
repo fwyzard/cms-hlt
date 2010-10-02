77b1414b849510d020c77a03fd6833a1409f2101
Andrea Bocci <andrea.bocci@cern.ch>
2010-10-02 01:04:08 +0000

    for paths accessing masked L1 Tech bits, use the updated TriggerResultsFilter functionality to access the decision before the L1 prescale

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-increase threshold on HLT_StoppedHSCP and rename to v2
+for paths accessing masked L1 Tech bits, use the updated TriggerResultsFilter functionality to access the decision before the L1 prescale
