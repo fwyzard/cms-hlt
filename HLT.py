eb374b23bf298262b5a4f6b71ebf82b76ed64527
Andrea Bocci <andrea.bocci@cern.ch>
2010-10-02 00:31:02 +0000

    drop version number from existing triggers: HLT_Ele12_SW_TightEleId_L1R_v2, HLT_Ele17_SW_L1R_v2, HLT_Ele17_SW_TightEleId_L1R_v1

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-change L1 seed for HLT_MET100 from ETM70 t ETM30, and increase prescales on HLT_L1ETT* paths to reduce rates to 1 Hz each
+drop version number from existing triggers: HLT_Ele12_SW_TightEleId_L1R_v2, HLT_Ele17_SW_L1R_v2, HLT_Ele17_SW_TightEleId_L1R_v1
