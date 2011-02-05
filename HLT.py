63e2fd45eacb789c25cc6c2e82f15ccd3d60c648
Andrea Bocci <andrea.bocci@cern.ch>
2011-02-05 18:53:01 +0000

    tentative import of HLT_QuadJet20_IsoPFTau_PFMHT_v1, and tentative change last hltActivityPhoton module to use global rechits

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-minimal changes to hltActivityPhoton modules ot use global unpacking (shape filters only)
+tentative import of HLT_QuadJet20_IsoPFTau_PFMHT_v1, and tentative change last hltActivityPhoton module to use global rechits
