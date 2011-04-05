75dbe98225f2b241d200d9a87a8c02e95ed7919f
Andrea Bocci <andrea.bocci@cern.ch>
2011-04-05 15:47:50 +0000

    change ZeroBias bit from tech 4 to algo 0

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-from Alex Mott: change the HT thresholds in the di-lepton+HT triggers from HT160 to HT150, adapted according to Jeff changes to EGamma sequences
+change ZeroBias bit from tech 4 to algo 0
