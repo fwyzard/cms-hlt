e0e029cfa195d22e17f5471fdfe42c410ac9bb6b
Andrea Bocci <andrea.bocci@cern.ch>
2011-04-05 12:31:07 +0000

    from Alex Mott: change the HT thresholds in the di-lepton+HT triggers from HT160 to HT150, adapted according to Jeff changes to EGamma sequences

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-Add new ES paths to smart prescale for Express
+from Alex Mott: change the HT thresholds in the di-lepton+HT triggers from HT160 to HT150, adapted according to Jeff changes to EGamma sequences
