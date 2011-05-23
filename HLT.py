2d5c8f407c973a3b971600bc31b325d8418ff3d9
Andrea Bocci <andrea.bocci@cern.ch>
2011-05-23 19:02:05 +0000

    remove minbias(tech 40) HLT path, and replace algo bit 126 with L1_ZeroBias (adapt prescales assuming 500Hz)

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-Fix ncandcut in hltEle10CaloIdTTrkIdVLCaloIsoVLTrkIsoVLTrackIsolFilterUnseeded
+remove minbias(tech 40) HLT path, and replace algo bit 126 with L1_ZeroBias (adapt prescales assuming 500Hz)
