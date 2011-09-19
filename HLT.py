8df26702eebf161a60fc7834a4a45065ea515c97
Juliette Alimena <juliette.alimena@cern.ch>
2011-09-19 19:25:01 +0000

    changed prescales in column 0 for HLT_CentralJet80_MET100 (from 5 to 1,  HLT_L2DoubleMu23_NoVertex (from 2 to 20), HLT_Photon90_CaloIdVL_IsoL (from 100 to 1)

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-imported AlCa_LumiPixels, event content, stream; added to datasets, set prescales
+changed prescales in column 0 for HLT_CentralJet80_MET100 (from 5 to 1,  HLT_L2DoubleMu23_NoVertex (from 2 to 20), HLT_Photon90_CaloIdVL_IsoL (from 100 to 1)
