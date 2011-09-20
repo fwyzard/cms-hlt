7224376cf575b1c49523514412550fffe56ceb62
Juliette Alimena <juliette.alimena@cern.ch>
2011-09-20 07:52:39 +0000

    unprescaled HLT_Ele32_CaloIdT_CaloIsoT_TrkIdT_TrkIsoT_Ele17 to 5e33

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-changed prescales in column 0 for HLT_CentralJet80_MET100 (from 5 to 1,  HLT_L2DoubleMu23_NoVertex (from 2 to 20), HLT_Photon90_CaloIdVL_IsoL (from 100 to 1)
+unprescaled HLT_Ele32_CaloIdT_CaloIsoT_TrkIdT_TrkIsoT_Ele17 to 5e33
