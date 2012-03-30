917b7e3efbd4c51d44cd9048f00294f13c23b2dd
Martin Grunewald <martin.grunewald@cern.ch>
2012-03-30 07:26:48 +0000

    Migration for DoAPVEmulatorCheck and set it to FALSE everywhere

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-revert hltSiStripRawToDigi.DoAPVEmulatorCheck to True for consistency with the previows behaviour (untl it can be adde and set to Fale in the on-demand modules, too)
+Migration for DoAPVEmulatorCheck and set it to FALSE everywhere
