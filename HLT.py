b150071d290efb7e94414c9ed24a2d1888ebb4c1
Andrea Bocci <andrea.bocci@cern.ch>
2012-03-30 00:07:16 +0000

    revert hltSiStripRawToDigi.DoAPVEmulatorCheck to True for consistency with the previows behaviour (untl it can be adde and set to Fale in the on-demand modules, too)

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-Adjust prescales for prescaled single-electron paths
+revert hltSiStripRawToDigi.DoAPVEmulatorCheck to True for consistency with the previows behaviour (untl it can be adde and set to Fale in the on-demand modules, too)
