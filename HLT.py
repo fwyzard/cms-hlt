698ee0819fb84d3c325745df8cb201222cd6d467
Andrea Perrotta <andrea.perrotta@cern.ch>
2014-02-01 15:03:43 +0000

    Fix jet cone in MetProducer's and SeedGeneratorFromRegionHitsEDProducer's; rename JEC as AK5, so that it will become easier modify them when needed

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-Fix JetCollection inputs in MuonIdProducer's
+Fix jet cone in MetProducer's and SeedGeneratorFromRegionHitsEDProducer's; rename JEC as AK5, so that it will become easier modify them when needed
