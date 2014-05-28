fc8be7e8d6eb1cb98e203f26325c91218516bb7f
Andrea Perrotta <andrea.perrotta@cern.ch>
2014-05-28 09:44:03 +0000

    Fix TTRHBuilder also in hltIter4PFlowPixelLessSeedsForTau; fix input tags in a few modules within HLTIterativeTrackingL3Muon sequence

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-Move TTRHBuilder parameter into the OrderedHitsFactoryPSet.GeneratorPSet
+Fix TTRHBuilder also in hltIter4PFlowPixelLessSeedsForTau; fix input tags in a few modules within HLTIterativeTrackingL3Muon sequence
