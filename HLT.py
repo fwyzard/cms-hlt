1f701e15daa15778590b285461e29064a0909a69
Andrea Bocci <andrea.bocci@cern.ch>
2014-10-10 18:23:48 +0000

    update L1 seed for HLT_Ele32_eta2p1_WP85_Gsf Top cross-triggers

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-modify all HLT_PhotonNNN triggers (NNN > 100 GeV) to use the L1 seed L1_SingleEG35, as a placeholder for a future, higher-threshold, unprescaled, non-isolated, non-eta-restricted seed
+update L1 seed for HLT_Ele32_eta2p1_WP85_Gsf Top cross-triggers
