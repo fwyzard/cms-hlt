caacd9a870871ac00b8484e9aaff5788ddbe350b
Andrea Bocci <andrea.bocci@cern.ch>
2014-10-10 18:14:00 +0000

    modify all HLT_PhotonNNN triggers (NNN > 100 GeV) to use the L1 seed L1_SingleEG35, as a placeholder for a future, higher-threshold, unprescaled, non-isolated, non-eta-restricted seed

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-update datasets
+modify all HLT_PhotonNNN triggers (NNN > 100 GeV) to use the L1 seed L1_SingleEG35, as a placeholder for a future, higher-threshold, unprescaled, non-isolated, non-eta-restricted seed
