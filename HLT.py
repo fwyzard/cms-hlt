b006c6df051e5652e060f1c54df26136b886a7af
Andrea Perrotta <andrea.perrotta@cern.ch>
2015-02-12 22:26:06 +0000

    Really fix the L1 seed: it had to be L1_SingleEg30

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-Substitute L1_SingleEG35er with L1_SingleEG40 in HLT_Diphoton28_14_R9Id85_OR_Iso50T80LCaloId24b40e_AND_HE10P5_R9Id50b80e_Mass50_Eta_1p5
+Really fix the L1 seed: it had to be L1_SingleEg30
