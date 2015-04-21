e7846f3a3b73b27f670961f63b2ad4ed86e9018a
Andrea Perrotta <andrea.perrotta@cern.ch>
2015-04-21 15:55:00 +0000

    Rename HLTIterativeTracking12ForBTagDisplacedJets sequence as HLTIterativeTrackingForBTagIter12, since it is identical

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-Fix L1 seeds for CMSHLT-118, and rename a few more L1s modules
+Rename HLTIterativeTracking12ForBTagDisplacedJets sequence as HLTIterativeTrackingForBTagIter12, since it is identical
