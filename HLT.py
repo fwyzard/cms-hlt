9081ae91c729d2cbe776220594eb994e01d3b4a8
Andrea Perrotta <andrea.perrotta@cern.ch>
2015-06-03 13:21:19 +0000

    Update and rename paths with CASTOR L1 seeds because of changes in GlobalTriggerMenu_L1Menu_Collisions2015_lowPU_v3 wrt v2

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-Move Prescaler after L1Seed module in AlCaEcalPi/Eta paths
+Update and rename paths with CASTOR L1 seeds because of changes in GlobalTriggerMenu_L1Menu_Collisions2015_lowPU_v3 wrt v2
