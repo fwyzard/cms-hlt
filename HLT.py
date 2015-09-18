930c1c48b1947685791a1d2208aafae26275e6c3
Andrea Perrotta <andrea.perrotta@cern.ch>
2015-09-18 15:25:03 +0000

    ALCAP0 stream selection update for AlCa_EcalEtaEBonly and cluster limits for timing, CMSHLT-584

--- HLT.py
+++ HLT.py
@@ -1,1 +1,1 @@
-Addition of HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v3 in the Express stream, CMSHLT-583
+ALCAP0 stream selection update for AlCa_EcalEtaEBonly and cluster limits for timing, CMSHLT-584
